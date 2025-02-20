19b0ce1ff138f8790738c948f432bcc34b3e9530
Joseph Parry <joeparrycreative@gmail.com>
2025-02-20 07:16:11 +0000

    Fix mergeAndSort function: corrected variable name, improved merging logic for performance. Don't use spread or concat

--- script.js
+++ script.js
@@ -15,10 +15,28 @@ function main() {
 // The spreading of the arrays arr1 and arr2 is done first, and then the merged array is filtered for zeros.
 // This reduces the number of operations when dealing with larger arrays by filtering the combined array instead of filtering both separately before merging.
 
+// function mergeAndSort(arr1, arr2) {
+//     let mergedArray = [...arr1, ...arr2];
+//     let filteredArray = mergedArray.filter(num => num !== 0);
+//     return filteredArray.sort((a, b) => a - b);
+// }
+
+// console.log(main());
+
 function mergeAndSort(arr1, arr2) {
-    let mergedArray = [...arr1, ...arr2];
-    let filteredArray = mergedArray.filter(num => num !== 0);
-    return filteredArray.sort((a, b) => a - b);
+    let merged = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== 0) merged.push(arr1[i]);
+    }
+
+    for (let i = 0; i < arr2.length; i++) {
+        merged.push(arr2[i]);
+    }
+
+    merged.sort((a, b) => a - b);
+
+    return merged;
 }
 
 console.log(main());
